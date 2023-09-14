69a11b106dfa692e62bae06919e9589af4c5ab9e
Mostafa Negim <mostafa.negim@gmail.com>
2023-09-14 12:38:35 +0330

    Add: open_host.go

---
+++ open_host.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type UserHandler interface {
+	IsUserPaymentActive(ctx context.Context, userID string) bool
+}
+
+type UserActiveResponse struct {
+	IsActive bool
+}
+
+func router(u UserHandler) {
+	m := chi.NewRouter()
+	m.Get("/user/{userID}/payment/active", func(writer http.ResponseWriter, request *http.Request) {
+		// check auth, etc
+		uID := chi.URLParam(request, "userID")
+		if uID == "" {
+			writer.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		isActive := u.IsUserPaymentActive(request.Context(), uID)
+		b, err := json.Marshal(UserActiveResponse{IsActive: isActive})
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		_, _ = writer.Write(b)
+	})
+}
