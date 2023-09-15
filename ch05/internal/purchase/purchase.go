8c9777aedecea3eb69aa081304cda4c3abb5a9c4
Mostafa Negim <mostafa.negim@gmail.com>
2023-09-15 22:19:47 +0330

    Add: validateAndEnrich

--- ch05/internal/purchase/purchase.go
+++ ch05/internal/purchase/purchase.go
@@ -4,6 +4,7 @@ import (
 	coffeeco "coffeeco/internal"
 	"coffeeco/internal/payment"
 	"coffeeco/internal/store"
+	"errors"
 	"github.com/Rhymond/go-money"
 	"github.com/google/uuid"
 	"time"
@@ -17,4 +18,23 @@ type Purchase struct {
 	PaymentMeans       payment.Means
 	timeOfPurchase     time.Time
 	CardToken          *string
+}
+
+func (p *Purchase) validateAndEnrich() error {
+	if len(p.ProductsToPurchase) == 0 {
+		return errors.New("purchase must consist of at least one product")
+	}
+	p.total = *money.New(0, "USD")
+
+	for _, v := range p.ProductsToPurchase {
+		newTotal, _ := p.total.Add(&v.BasePrice)
+		p.total = *newTotal
+	}
+	if p.total.IsZero() {
+		return errors.New("likely mistake; purchase should never be 0. Please validate")
+	}
+	p.id = uuid.New()
+	p.timeOfPurchase = time.Now()
+
+	return nil
 }
